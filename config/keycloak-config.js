31d292d2b6536fe7bc8f8c75fa5f0f080a485dd2
Yuanming Chen <yuanming@hotmail.com>
2021-03-22 17:23:50 -0600

    REST API protection with Keycloak

---
+++ config/keycloak-config.js
@@ -0,0 +1,45 @@
+var session = require('express-session');
+var Keycloak = require('keycloak-connect');
+
+let _keycloak;
+
+var keycloakConfig = {
+    clientId: 'nodejs-microservice',
+    bearerOnly: true,
+    serverUrl: "http://localhost:8080/auth",
+    realm: "Demo-Realm",
+    credentials: {
+      secret: "1c6fe190-835f-40d7-ac8d-abc232623353"
+    }
+};
+
+function initKeycloak(app) {
+    if (_keycloak) {
+        console.warn("Trying to init Keycloak again!");
+        return _keycloak;
+    } 
+    else {
+        console.log("Initializing Keycloak...");
+        var memoryStore = new session.MemoryStore();
+        app.use(session({
+            secret: 'some secret',
+            resave: false,
+            saveUninitialized: true,
+            store: memoryStore
+          }));
+        _keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
+        return _keycloak;
+    }
+}
+
+function getKeycloak() {
+    if (!_keycloak){
+        console.error('Keycloak has not been initialized. Please called init first.');
+    } 
+    return _keycloak;
+}
+
+module.exports = {
+    initKeycloak,
+    getKeycloak
+};
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
-var testController = require('./controller/test-controller.js');
 var app = express();
+
+const keycloak = require('./config/keycloak-config.js').initKeycloak(app);
+app.use(keycloak.middleware());
+
+var testController = require('./controller/test-controller.js');
+app.use('/test', testController);
 
 app.get('/', function(req, res){
    res.send("Server is up!");
 });
-
-app.use('/test', testController);
 app.listen(3000);
